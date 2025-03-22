b7ede3b5866aff8d5199d977892cbee634e8ab6b
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-03-22 13:26:15 +0500

    Add auth route in main

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from 'hono'
+import authRoutes from './auth/auth.controller';
+import { ENV_HEALTH } from './_Config/Environment';
+import { logger } from 'hono/logger';
 
 const app = new Hono()
 
+// Health Check
+app.use(logger())
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text(`hello form hono , env health: ${ENV_HEALTH}`);
+});
+
+// Routes
+app.route("/",authRoutes)
+
 
 export default app
